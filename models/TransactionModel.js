ad41e845f063f245936655dcfb587f79028649ab
TharunYetti <tharunyetti24@gmail.com>
2024-09-19 19:37:32 +0530

    Added TransactionModel.js file

---
+++ models/TransactionModel.js
@@ -0,0 +1,22 @@
+const TransactionsSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        require: true,
+        default: Date.now
+    },
+    transaction_type: {
+        type: String,
+        enum: ['debit','credit'],
+        require: true
+    },
+    amount: {
+        type: Number,
+        require: true
+    },
+    purpose: {
+        type: String,
+        require: true
+    }
+});
+
+export const transactions = new mongoose.model("transactions",TransactionsSchema);
