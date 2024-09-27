146a11877209eed5fc220a7e28266a6246ba503f
TharunYetti <tharunyetti24@gmail.com>
2024-09-27 21:18:00 +0530

    fixing bugs

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import offusers from "./Routes/offuserRoutes.js";
 import volunteerRoutes from "./Routes/volunteerRoutes.js";
-import { transactionUpdate } from "./Controllers/transactionController.js";
 import cors from 'cors';
 import eventRoute from "./Routes/eventRoute.js";
 const app = express();
@@ -27,7 +26,8 @@ app.get("/",(req,res)=>{
 app.use("/api/event",eventRoute);
 app.use('/api/users/offusers',offusers);
 app.use('/api/volunteers',volunteerRoutes);
-app.use('/api/transaction',transactionUpdate);
+app.use('/api/transaction',transactionsRoute);
+app.use("/api/testimonial",testimonialRoute);
 
 app.listen(port,()=>{
     console.log(`Server running at http://localhost:${port}`);
