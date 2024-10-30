ff65cc21045da3bf139123b9a79bf6da1ed5e8fe
Rushendra Kumar <rushi17092004@gmail.com>
2024-10-30 22:02:55 +0530

    Added routes to get all users and update users

--- controllers/offUserController.ts
+++ controllers/offUserController.ts
@@ -27,6 +27,24 @@ class OffUserController{
     }
 
   }
+  async getAllUsers(req:Request,res:Response){
+    try{
+      const data = await offUserModel.find();
+      res.status(200).json(data);
+    }
+    catch(error){
+      res.status(400).json({success:false,message:error.message});
+    }
+  }
+  async updateUsers(req:Request,res:Response){
+    try{
+      const uid = req.params.id;
+      const userExist = await offUserModel.findByIdAndUpdate(uid,req.body);
+      res.status(200).json({message:"success"});
+    }catch(e){
+      res.status(400).json(e);
+    }
+  }
     async getUserData(req: Request, res: Response){
       try{
         const data = await offUserService.getUserData(req.params.id);
